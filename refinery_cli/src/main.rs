ec25669b83ee8a96047d1e5ea6f94068a59dd1ea
João Oliveira <hello@jxs.pt>
2020-03-31 15:11:51 +0100

    update and lock human-panic

--- refinery_cli/src/main.rs
+++ refinery_cli/src/main.rs
@@ -18,7 +18,6 @@ fn main() -> Result<(), Error> {
         version: VERSION.into(),
         authors: "Katharina Fey <kookie@spacekookie.de>, João Oliveira <hello@jxs.pt>".into(),
         homepage: "https://github.com/rust-db/refinery/".into(),
-        repository: "https://github.com/rust-db/refinery/".into(),
     });
 
     let mut builder = Builder::new();
