e71dac37743e15856a1ff192e5b14fd2eccafbb3
Kirill Bulatov <mail4score@gmail.com>
2020-05-20 13:48:51 +0300

    Bump mysql version

--- refinery_core/src/drivers/mysql.rs
+++ refinery_core/src/drivers/mysql.rs
@@ -2,14 +2,22 @@ use crate::traits::sync::{Migrate, Query, Transaction};
 use crate::Migration;
 use chrono::{DateTime, Local};
 use mysql::{
-    error::Error as MError, Conn, IsolationLevel, PooledConn, Transaction as MTransaction,
+    error::Error as MError, prelude::Queryable, Conn, IsolationLevel, PooledConn,
+    Transaction as MTransaction, TxOpts,
 };
+
+fn get_tx_opts() -> TxOpts {
+    TxOpts::default()
+        .set_with_consistent_snapshot(true)
+        .set_access_mode(None)
+        .set_isolation_level(Some(IsolationLevel::RepeatableRead))
+}
 
 fn query_applied_migrations(
     transaction: &mut MTransaction,
     query: &str,
 ) -> Result<Vec<Migration>, MError> {
-    let rows = transaction.query(query)?;
+    let rows = transaction.query_iter(query)?;
     let mut applied = Vec::new();
     for row in rows {
         let row = row?;
@@ -36,11 +44,10 @@ impl Transaction for Conn {
     type Error = MError;
 
     fn execute(&mut self, queries: &[&str]) -> Result<usize, Self::Error> {
-        let mut transaction =
-            self.start_transaction(true, Some(IsolationLevel::RepeatableRead), None)?;
+        let mut transaction = self.start_transaction(get_tx_opts())?;
         let mut count = 0;
         for query in queries.iter() {
-            transaction.query(query)?;
+            transaction.query_iter(query)?;
             count += 1;
         }
         transaction.commit()?;
@@ -52,12 +59,11 @@ impl Transaction for PooledConn {
     type Error = MError;
 
     fn execute(&mut self, queries: &[&str]) -> Result<usize, Self::Error> {
-        let mut transaction =
-            self.start_transaction(true, Some(IsolationLevel::RepeatableRead), None)?;
+        let mut transaction = self.start_transaction(get_tx_opts())?;
         let mut count = 0;
 
         for query in queries.iter() {
-            transaction.query(query)?;
+            transaction.query_iter(query)?;
             count += 1;
         }
         transaction.commit()?;
@@ -67,8 +73,7 @@ impl Transaction for PooledConn {
 
 impl Query<Vec<Migration>> for Conn {
     fn query(&mut self, query: &str) -> Result<Vec<Migration>, Self::Error> {
-        let mut transaction =
-            self.start_transaction(true, Some(IsolationLevel::RepeatableRead), None)?;
+        let mut transaction = self.start_transaction(get_tx_opts())?;
         let applied = query_applied_migrations(&mut transaction, query)?;
         transaction.commit()?;
         Ok(applied)
@@ -77,8 +82,7 @@ impl Query<Vec<Migration>> for Conn {
 
 impl Query<Vec<Migration>> for PooledConn {
     fn query(&mut self, query: &str) -> Result<Vec<Migration>, Self::Error> {
-        let mut transaction =
-            self.start_transaction(true, Some(IsolationLevel::RepeatableRead), None)?;
+        let mut transaction = self.start_transaction(get_tx_opts())?;
         let applied = query_applied_migrations(&mut transaction, query)?;
         transaction.commit()?;
         Ok(applied)
