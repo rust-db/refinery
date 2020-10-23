04541f9178695d0279ad921bf85b918ac54275d0
Miro Andrin Debrunner <miroad@pop-os.localdomain>
2020-10-23 21:48:37 +0200

    Added  code to detect if there are multiple midgrations with the same version number.

--- refinery_core/src/util.rs
+++ refinery_core/src/util.rs
@@ -1,6 +1,6 @@
 use crate::error::{Error, Kind};
-use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
+use std::{collections::HashSet, ffi::OsStr};
 use walkdir::{DirEntry, WalkDir};
 
 use regex::Regex;
@@ -37,30 +37,43 @@ pub fn find_migration_files(
         )
     })?;
 
-    let file_paths = WalkDir::new(location)
+    let file_paths: Vec<PathBuf> = WalkDir::new(location)
         .into_iter()
         .filter_map(Result::ok)
         .map(DirEntry::into_path)
-        // filter by migration file regex
-        .filter(
-            move |entry| match entry.file_name().and_then(OsStr::to_str) {
-                Some(file_name) => {
-                    if re.is_match(file_name) {
-                        true
+        .collect();
+
+    let mut occurence: HashSet<String> = HashSet::with_capacity(file_paths.len());
+    let mut result : Vec<PathBuf> = Vec::with_capacity(file_paths.len());
+
+    for entry in &file_paths {
+        if let Some(file_name) = entry.file_name().and_then(OsStr::to_str) {
+            match re.captures(file_name) {
+                Some(captures) => {
+                    // Unwrap should always be safe due to how the regex is defined.
+                    // The first and second matching groups are not optional, so it must
+                    // at least have those two for the captures to be Some.
+                    let version = captures.get(2).unwrap();
+
+                    if occurence.insert(version.as_str().to_owned()) {
+                        result.push(entry.to_owned());
                     } else {
-                        log::warn!(
+                        return Err(Error::new(Kind::DuplicateVersion, None));
+                    }
+                }
+                ,None => {
+                    log::warn!(
                             "File \"{}\" does not adhere to the migration naming convention. Migrations must be named in the format [U|V]{{1}}__{{2}}.sql or [U|V]{{1}}__{{2}}.rs, where {{1}} represents the migration version and {{2}} the name.",
                             file_name
                         );
-
-                        false
-                    }
                 }
-                None => false,
-            },
-        );
+                
+            }
+        }
+    }
 
-    Ok(file_paths)
+
+    Ok(result.into_iter())
 }
 
 #[cfg(test)]
@@ -150,5 +163,19 @@ mod tests {
 
         let mut mods = find_migration_files(migrations_dir, MigrationType::Sql).unwrap();
         assert!(mods.next().is_none());
+    }
+
+
+    #[test]
+    fn returns_error_if_two_files_have_same_version() {
+        let tmp_dir = TempDir::new().unwrap();
+        let migrations_dir = tmp_dir.path().join("migrations");
+        fs::create_dir(&migrations_dir).unwrap();
+        let sql1 = migrations_dir.join("V1__first.sql");
+        fs::File::create(&sql1).unwrap();
+        let sql2 = migrations_dir.join("V1__second.sql");
+        fs::File::create(&sql2).unwrap();
+
+        assert!(find_migration_files(migrations_dir, MigrationType::Sql).is_err());
     }
 }
