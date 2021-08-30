f5386c0d9e44ae423f173a354649d8b8b06a2d7a
Omid Rad <omidmr@gmail.com>
2021-08-30 19:38:58 +0200

    Remove mod.rs in example

--- examples/modules/main.rs
+++ examples/modules/main.rs
@@ -1,9 +1,13 @@
 use rusqlite::Connection;
 
-mod migrations;
+mod embedded {
+    use refinery::embed_migrations;
+    // by default there is no need to specify the location
+    // we need to specify here because there is also another migrations dir in tests
+    embed_migrations!("refinery/examples/modules/migrations");}
 
 fn main() {
     let mut conn = Connection::open_in_memory().unwrap();
 
-    migrations::runner().run(&mut conn).unwrap();
+    embedded::migrations::runner().run(&mut conn).unwrap();
 }
--- examples/modules/migrations/mod.rs
+++
@@ -1,5 +0,0 @@
-use refinery::embed_migrations;
-
-//by default there is no need to specify the location
-//we need to specify here because there is also another migrations dir in tests
-embed_migrations!("refinery/examples/modules/migrations");
